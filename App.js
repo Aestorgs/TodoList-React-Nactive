6dae11990859bb804fbe280f14668f745d33f942
Aestorgs <dylan.bo@institutsolacroup.com>
2023-09-07 19:10:53 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Tasks from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Tâches d'aujourd'hui */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's task </Text>
+        <View style={styles.items}>
+          {/* C'est là que la tâche ira ! */}
+          <Tasks Text="Task 1" />
+          <Tasks Text="Task 2" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  items: {
+    marginTop: 30,
   },
 });
